73899c33858e092eb40c59094ce95863d645d86e
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2024-02-21 02:17:53 +0200

    Add all required folders for backend and DB schema for users model and patients model

---
+++ back-end/models/user-model.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  assignedPatients: [{ type: Schema.Types.ObjectId, ref: 'Patient' }],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+  photo: {
+    type: String,
+  },
+});
+
+module.exports = userSchema;
