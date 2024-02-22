e3d3e034ca30e7c71262c8f42a8de3a457450a67
Ammar-Khaled <ammar.khaled.github@gmail.com>
2024-02-22 16:51:51 +0000

    Set up patient router and controller

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const usersRouter = require('./routes/user-routes');
+const patientRouter = require('./routes/patient-routes');
 // eslint-disable-next-line import/no-extraneous-dependencies
 require('dotenv').config();
 
@@ -9,6 +10,7 @@ const port = process.env.PORT;
 const app = express();
 app.use(express.json());
 app.use('/api/users', usersRouter);
+app.use('/api/patients', patientRouter);
 
 mongoose.connect(url).then(() => {
   console.log('successfully connected to mongodb');
