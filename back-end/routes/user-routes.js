3b1c5c2a581840784995388c5af7292894d4ebd0
Ammar-Khaled <ammar.khaled.github@gmail.com>
2024-02-28 21:59:55 +0200

    [Feature] Allow user to upload a profile photo when register

--- back-end/routes/user-routes.js
+++ back-end/routes/user-routes.js
@@ -1,20 +1,29 @@
 const express = require('express');
 const userController = require('../controllers/UserController');
 const router = express.Router();
-const {validateUser} = require('../middlewares/validationSchemas');
+const { validateUser } = require('../middlewares/validationSchemas');
+const multer = require('multer');
 
+router.route('/').get(userController.getAllUsers);
 
-router.route('/')
-  .get(userController.getAllUsers);
+router.route('/:id').get(userController.getUser);
 
-router.route('/:id')
-  .get(userController.getUser);
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    console.log('file', file);
+    cb(null, 'uploads');
+  },
+  filename: (req, file, cb) => {
+    let photo = `user-${Date.now()}.${file.mimetype.split('/')[1]}`;
+    cb(null, photo);
+    req.photo = photo;
+  },
+});
+const upload = multer({ storage });
+router
+  .route('/register')
+  .post(upload.single('photo'), userController.registerUser);
 
-
-router.route('/register')
-  .post(validateUser(), userController.registerUser);
-
-router.route('/login')
-  .post(userController.loginUser);
+router.route('/login').post(userController.loginUser);
 
 module.exports = router;
