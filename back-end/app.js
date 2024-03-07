b7de89f26dcb2effba2114554b7064d340dbd546
Ammar-Khaled <ammar.khaled.github@gmail.com>
2024-03-07 22:57:41 +0200

    Export app for testing and update dotenv to dotenv-flow

--- back-end/app.js
+++ back-end/app.js
@@ -5,7 +5,7 @@ const patientRouter = require('./routes/patient-routes');
 const jSend = require('jsend');
 const createError = require('http-errors');
 const cors = require('cors');
-require('dotenv').config();
+require('dotenv-flow').config();
 const path = require('path');
 
 const url = process.env.MONGO_URL;
@@ -38,3 +38,5 @@ mongoose.connect(url).then(() => {
 app.listen(port, () => {
   console.log(`Connected to port ${port}`);
 });
+
+module.exports = app;
