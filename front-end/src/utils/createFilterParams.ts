8b0fe378b4d2bd2ed17d2dde3e0af0c47b0d4dc0
MohammedGhaly <medooghaly7@gmail.com>
2024-03-14 17:14:46 +0200

    adding FilterParams Type

--- front-end/src/utils/createFilterParams.ts
+++ front-end/src/utils/createFilterParams.ts
@@ -1,29 +1,33 @@
 import { Filters } from "../interfaces/Filters";
 
+type FilterParams = {
+  departments?: Array<string>;
+  sex?: string;
+  isEmergency?: boolean;
+  medicalCompromised?: boolean;
+};
+
 function createFilterParams(filters: Filters) {
-  const filterParams = {};
+  const filterParams: FilterParams = {};
 
   filterParams["departments"] = Object.keys(filters.department).filter(
     (key) => filters.department[key]
   );
   if (filters.sex.Male !== filters.sex.Female) {
-    if (filters.sex.Male)
-      filterParams["sex" as keyof typeof filterParams] = "male";
-    else filterParams["sex" as keyof typeof filterParams] = "female";
+    if (filters.sex.Male) filterParams.sex = "male";
+    else filterParams.sex = "female";
   }
   if (filters.emergency.Emergency !== filters.emergency.notEmergency) {
-    if (filters.emergency.Emergency)
-      filterParams["isEmergency" as keyof typeof filterParams] = true;
-    else filterParams["isEmergency" as keyof typeof filterParams] = false;
+    if (filters.emergency.Emergency) filterParams.isEmergency = true;
+    else filterParams.isEmergency = false;
   }
   if (
     filters.medicalCompromised.MedicalCompromised !==
     filters.medicalCompromised.NotMedicalCompromised
   ) {
     if (filters.medicalCompromised.MedicalCompromised)
-      filterParams["medicalCompromised" as keyof typeof filterParams] = true;
-    else
-      filterParams["medicalCompromised" as keyof typeof filterParams] = false;
+      filterParams.medicalCompromised = true;
+    else filterParams.medicalCompromised = false;
   }
 
   return filterParams;
